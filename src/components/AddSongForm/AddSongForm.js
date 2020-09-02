e41c1055b8d3cfb0263d4cc9040805412400fcbe
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 10:58:10 -0600

    Add onChange handler to add song form

--- src/components/AddSongForm/AddSongForm.js
+++ src/components/AddSongForm/AddSongForm.js
@@ -15,17 +15,28 @@ class AddSongForm extends Component {
     return (
       <section className='add-song-right'>
         <h2>Add Song to Playlist</h2>
-        <form className='AddSongForm'>
+        <form className='AddSongForm' onChange={(event) => this.handleChange(event)}>
           <label for='songName'>Song Name:</label>
           <input type='text' name='songName' id='songName' value={this.state.songName}/>
           <label for='artistName'>Artist:</label>
           <input type='text' name='artistName' id='artistName' value={this.state.artistName}/>
           <label for='link'>YouTube link:</label>
           <input type='text' name='link' id='link' value={this.state.link}/>
-          <input type='submit' value='Add song' id='add-song-btn'/>
+          <input type='submit' value='Add song' id='add-song-btn' onChange={this.addNewSong}/>
         </form>
       </section>
     )
+  }
+
+  handleChange = (event) => {
+    const input = event.target.name; 
+    const value = event.target.value; 
+    this.setState({[input]: value}); 
+  }
+
+  //MOVE TO APP 
+  addNewSong = () => {
+
   }
 }
 
