ca3d7da30d244584579bc543027c1b41d8646ebf
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 10:10:31 -0600

    Make additional font stlying tweaks

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -6,7 +6,7 @@ const Song = ({ songName, artistName, link, id}) => {
   return (
     <article className='Song' id={id}>
       <h3>{songName}</h3>
-      <h4>{artistName}</h4>
+      <h4>Artist: {artistName}</h4>
       <a href={link} target='_blank' rel='noopener noreferrer'>Listen now!</a>
     </article>
   )
