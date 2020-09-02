cb7a8aab09558aa1343ece1de1de2891a2147c8e
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 11:54:55 -0600

    Add App integration test for filling out form and seeing song appear on DOM

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('true should be true', () => {
-  expect(true).toEqual(true);
-});
+describe('App component', () => {
+  it('should allow a user to add a new song', () => {
+
+    render(
+      <App /> 
+    )
+
+    const songInput = screen.getByPlaceholderText('Song name');
+    const artistInput = screen.getByPlaceholderText('Artist');
+    const linkInput = screen.getByPlaceholderText('Link');
+    const AddBtn = screen.getByText('Add song');
+
+    fireEvent.change(songInput, { target: { value: 'Break My Heart Again' } });
+    fireEvent.change(artistInput, { target: { value: 'FINNEAS' } });
+    fireEvent.change(linkInput, { target: { value: 'https://www.youtube.com/watch?v=iS7gY_vdAn8' } });
+
+    fireEvent.click(AddBtn); 
+
+    const songTitle = screen.getByText('Break My Heart Again');
+    const artist = screen.getByText('Artist: FINNEAS');
+
+    expect(songTitle).toBeInTheDocument(); 
+    expect(artist).toBeInTheDocument(); 
+  })
+})
